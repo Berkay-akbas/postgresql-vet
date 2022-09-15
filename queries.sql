0d282be522378e036f0520d8da72b1af6564b396
Berkay Akbas <berkayakbas@gmail.com>
2022-09-15 18:58:03 +0300

    create new tables and queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*Queries that provide answers to the questions FROM all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
@@ -37,5 +37,19 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
-SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP by species;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+SELECT name as Melody_Pond_animals FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.id = 4;
+
+SELECT animals.name as pokemons FROM animals JOIN species ON animals.species_id = species.id WHERE species.id = 1;
+
+SELECT o.full_name, string_agg(a.name, ',') AS animals FROM owners o full JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name;
+
+SELECT s.name as species, count(*) as number_of_animals FROM species s JOIN animals a ON s.id = a.species_id GROUP BY s.name;
+
+SELECT o.full_name, string_agg(a.name, ',') AS digimons FROM owners o full JOIN animals a ON o.id = a.owner_id WHERE o.id = 2 AND a.species_id = 2 GROUP BY o.full_name;
+
+SELECT o.full_name, string_agg(a.name, ',') AS escaped_animals FROM owners o full JOIN animals a ON o.id = a.owner_id WHERE o.id = 5 AND a.escape_attempts = 0 GROUP BY o.full_name;
+
+SELECT o.full_name, count(*) AS number_of_animals FROM owners o FULL JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name ORDER BY number_of_animals DESC limit 1;
--- schema.sql
+++ schema.sql
@@ -9,3 +9,34 @@ neutered BOOLEAN,
 weight_kg DECIMAL,
 species TEXT,
 );
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR,
+    age INT,
+    PRIMARY KEY (id)
+);
+
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP column species;
+
+ALTER TABLE animals ADD column species_id INT
+
+ALTER TABLE animals ADD column owner_id INT
+
+ALTER TABLE animals 
+   ADD CONSTRAINT fk_species
+    FOREIGN KEY (species_id) 
+     REFERENCES species(id);
+
+ALTER TABLE animals 
+   ADD CONSTRAINT fk_owners
+    FOREIGN KEY (owner_id) 
+      REFERENCES owners(id)
