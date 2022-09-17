4743d04a7798d28f3a9759388ac8b3165a19484e
Berkay Akbas <berkayakbas@gmail.com>
2022-09-17 12:56:44 +0300

    Created new tables with the provided data, wrote quieries for the questions

--- queries.sql
+++ queries.sql
@@ -53,3 +53,30 @@ SELECT o.full_name, string_agg(a.name, ',') AS digimons FROM owners o full JOIN 
 SELECT o.full_name, string_agg(a.name, ',') AS escaped_animals FROM owners o full JOIN animals a ON o.id = a.owner_id WHERE o.id = 5 AND a.escape_attempts = 0 GROUP BY o.full_name;
 
 SELECT o.full_name, count(*) AS number_of_animals FROM owners o FULL JOIN animals a ON o.id = a.owner_id GROUP BY o.full_name ORDER BY number_of_animals DESC limit 1;
+
+
+SELECT vets.name, animals.name, visits.date_of_visit  FROM animals JOIN visits ON visits.animal_id = animals.id JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'William Tatcher' ORDER BY visits.date_of_visit DESC LIMIT 1;  
+
+SELECT vets.name, COUNT(animals.name) FROM animals JOIN visits ON visits.animal_id = animals.id JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'Stephanie Mendez' GROUP BY vets.name;
+
+SELECT vets.name, S.name as Speciality FROM vets
+LEFT JOIN specializations ON vets.id = specializations.vets_id LEFT JOIN species S ON S.id = specializations.species_id; 
+
+SELECT animals.name, visits.date_of_visit  FROM animals JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'Stephanie Mendez' AND visits.date_of_visit BETWEEN '2020-4-1' AND '2020-8-30';
+
+SELECT a.name, COUNT(A.name) as number_of_visits FROM animals A JOIN visits vi ON vi.animal_id = A.id JOIN vets ve ON vi.vets_id = Ve.id GROUP BY A.name ORDER BY number_of_visits DESC LIMIT 1;
+
+SELECT animals.name as animal_name, visits.date_of_visit  
+FROM animals JOIN visits ON visits.animal_id = animals.id JOIN vets ON visits.vets_id = vets.id WHERE vets.name = 'Maisy Smith' ORDER BY visits.date_of_visit LIMIT 1;
+
+SELECT animals.name AS animal_name, species.name as specie, animals.date_of_birth AS birth_date, animals.escape_Attempts, animals.neutered, 
+animals.weight_kg, owners.full_name AS Owner_name, vets.name as Vet_name, vets.age AS Vet_age, vets.date_of_graduation, visits.date_of_visit  FROM animals JOIN visits ON visits.animal_id = animals.id JOIN vets ON visits.vets_id = vets.id
+JOIN species ON animals.species_id = species.id
+JOIN owners ON animals.owner_id = owners.id
+ORDER BY visits.date_of_visit DESC
+LIMIT 1;
+
+SELECT count(*) FROM visits LEFT JOIN animals ON animals.id = visits.animal_id LEFT JOIN vets ON vets.id = visits.vets_id WHERE animals.species_id NOT IN (select animals.species_id FROM specializations WHERE specializations.vets_id = vets.id);
+
+SELECT species.name, count(*) from visits LEFT JOIN animals ON animals.id = visits.animal_id LEFT JOIN species ON animals.species_id = species.id LEFT JOIN vets ON vets.id = visits.vets_id where vets.name = 'Maisy Smith' GROUP BY species.name ORDER BY species.name ASC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -40,3 +40,36 @@ ALTER TABLE animals
    ADD CONSTRAINT fk_owners
     FOREIGN KEY (owner_id) 
       REFERENCES owners(id)
+
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+    species_id INT,
+    vets_id INT,
+    CONSTRAINT species_key
+        FOREIGN KEY (species_id)
+            REFERENCES species(id),
+    CONSTRAINT vets_key
+        FOREIGN KEY (vets_id)
+            REFERENCES vets(id)
+                ON DELETE CASCADE        
+);
+
+CREATE TABLE visits (
+    animal_id INT,
+    vets_id INT,
+    date_of_visit DATE,
+    CONSTRAINT animals_key
+        FOREIGN KEY (animal_id)
+            REFERENCES animals(id),
+    CONSTRAINT vets_key
+        FOREIGN KEY (vets_id)
+            REFERENCES vets(id)        
+);
